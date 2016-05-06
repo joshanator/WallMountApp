ac484106d1f7cef79184751eeff0b8e129266494
The Josh <peterson.joshua.j@gmail.com>
2016-05-06 17:31:52 -0400

    5-6

---
+++ UIeditor/index.php
@@ -0,0 +1,78 @@
+<link type="text/css" rel="stylesheet" href="/style.css"/>
+<script src="/jquery.js"></script>
+<script src="/script.js"></script>
+<?php
+    $db = mysqli_connect('localhost','WebApp','1000001870','WallMount');
+	
+	if ($db->connect_errno) {
+    printf("Connect failed: %s\n", $db->connect_error);
+	}
+
+	if(isset($_GET['id']))
+	{
+		$id = $_GET['id'];
+		
+		 $result = mysqli_query($db, 
+     	"CALL dbo.VERIFY(" . $id . ")") or die("Query fail: " . mysqli_error());
+		
+
+
+		
+		echo $result;
+		
+		if($result=="1")
+		{
+			echo '<style> #infopage{z-index:10;}</style>';
+		}
+		else
+		{
+			echo '<style> #main{z-index:10;}</style>';
+		}
+	}
+	else
+	{
+		echo '<style> #main{z-index:10;}</style>';
+	}
+	mysqli_close($db);
+?>
+
+<html>
+<head>
+  <meta charset="utf-8">
+</head>
+
+	
+<body>
+
+<div id="main" class="info">
+
+	<button>New User</button>
+	<button onclick="returning()">Returning</button> 
+	
+</div>	
+	
+
+<div id="returning" class="info">
+	Enter ID 
+	</br>
+	<input id="ID">
+	<button onclick="login(); ">Submit</button>
+</div>
+
+<div id="infopage" class="info">
+	Edit settings
+	</br>
+	Calender 
+	<input type="checkbox" value="true" class="calender">
+	</br>
+	Weather
+	<input type="checkbox" value="true" class="weather">
+	</br>
+</div>
+
+	
+	
+	
+	
+</body>
+</html>
--- UIeditor/script.js
+++ UIeditor/script.js
@@ -1,5 +1,26 @@
+var id=0;
+
 function returning()
 {
 	$("#main").css("z-index","0");
 	$("#returning").css("z-index","10");
+}
+
+function infopage()
+{
+	$("#main").css("z-index","0");
+	$("#infopage").css("z-index","10");
+}
+
+function getid()
+{
+	id=document.getElementById("ID").value;
+}
+
+
+
+function login()
+{
+	id=document.getElementById("ID").value;
+	window.location ="http://localhost:8080/index.php/?id="+id;
 }
