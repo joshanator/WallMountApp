bbef52c647926e9347f7fe848507516202c9837f
David Thames <davidcthames@gmail.com>
2016-06-07 09:25:34 -0400

    Updates to editor

---
+++ UIeditor/construct.php
@@ -0,0 +1,3 @@
+<?php
+echo '<html><body>' + $_POST["name"] + '</body></html>');
+?>
--- UIeditor/editor.js
+++ UIeditor/editor.js
@@ -7,6 +7,10 @@ var step = 1;
 
 function page(direc) {
 	step += direc;
+	if(step == 3)
+		construct();
+	if(step < 1) step = 1;
+	if(step > 2) step = 2;
 	
 	$(".open").css("left", (-1 * direc * 100) + "%");
 	$(".open").removeClass("open");
@@ -22,13 +26,25 @@ window.onload = function() {
 }
 
 function selectionBox(num) {
-	return "<div style='background-color:white; width:" + layoutProp[$(".layout.active").data("num")].width[num] + "; height:" + layoutProp[$(".layout.active").data("num")].height[num] + "'><select> <option value='calendar'>Calendar</option> <option value='weather'>Weather</option> <option value='time'>Time</option> </select></div>"
+	return "<div class='card' style='background-color:white; width:500px; margin-left:auto; margin-right:auto;'> <select> <option value='calendar.html'>Calendar</option> <option value='weather.html'>Weather</option> <option value='time.html'>Time</option> </select></div>"
 }
 
 
 function step2() {
-	for(var i = 0; i < $(".layout.active").data("num"); i++)
-		$(".step2").append(selectionBox(i));
+	$("div.step2").html("");
+	for(var i = 0; i < $(".layout.selected").data("num"); i++)
+		$("div.step2").append(selectionBox(i));
+}
+
+function construct() {
+	var num  = $(".layout.selected").attr("id");
+	var output = layout[num - 1][0] ;
+	for(var i = 0; i < layoutProp[num].num; i++) {
+		output += $("select")[i].selected + layout[num - 1][i + 1];
+	}
+	console.log(output);
+	localStorage.setItem("WallMountPage", output);
+	//$.post(construct.php, {page: output});
 }
 
 
@@ -54,3 +70,24 @@ var layoutProp = [
 		height:["100%", "100%", "100%"]
 	}
 ]
+
+var layout = [
+	[
+		"FirstPART(",
+		")SecondPART(",
+		")ThirdPART(",
+		")fourthPART(",
+		")fithPart"
+	],
+	[
+		
+	],
+	[
+		
+	],
+	[
+		
+	]
+]
+
+
